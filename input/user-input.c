67d470655a7d95fa8615a205ca36640c61bc04b2
Codinglynd <ethanrocks500@proton.me>
2025-08-16 19:20:17 -0500

    added a folder called input and put user-input.c in it

---
+++ input/user-input.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int age = 0;
+    float gpa = 0.0f;
+    char grade = '\0';
+    char name[64] = "";
+
+    printf("Enter Your Age: ");
+    scanf("%d", &age);
+
+    
+    printf("Enter Your GPA: ");
+    scanf("%f", &gpa);
+
+    printf("Enter Your Grade: ");
+    scanf(" %c", &grade);
+
+    getchar();
+    printf("Enter your full name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    printf("\n%s\n", name);
+    printf("%d\n", age);
+    printf("%.2f\n", gpa);
+    printf("%c\n\n", grade);
+
+    return 0;
+}
