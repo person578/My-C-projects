7c926e918d9989bd59c46429ad6457f4de15befc
Codinglynd <ethanrocks500@proton.me>
2025-09-05 19:34:51 -0500

    added everything back in also got rid of the bool header file

--- input/if-statments.c
+++ input/if-statments.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main()
 {
 
+  int age = 0;
 
+  printf("How old are you? ");
+  scanf("%d", &age);
+
+  if(age >= 18){
+    printf("You're an adult");
+  }
+
+  else if(age < 0){
+    printf("You have not been born yet");
+  }
+
+  else if(age == 0){
+    printf("You have just been born ");
+  }
+
+  else {
+    printf("You're not an adult");
+  }
 
   return 0;
 }
