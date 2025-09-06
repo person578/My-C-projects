f62f31ea087bb867b6efd083e7b587afef9d93a7
Codinglynd <ethanrocks500@proton.me>
2025-09-06 13:15:55 -0500

    made a new folder for calculator files

---
+++ input/calculators/comp-interest-calc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double principal = 0.0;
+    double rate = 0.0;
+    int years = 0;
+    int timesCompounded = 0;
+    double total = 0.0;
+
+    printf("Compound Interest Calculator\n");
+
+    printf("Enter the principal ");
+    scanf("%lf", &principal);
+
+    printf("Enter the interest rate %%");
+    scanf("%lf", &rate);
+    rate = rate / 100;
+
+    printf("Enter the # of years : ");
+    scanf("%d", &years);
+
+    printf("Enter # of years compound ");
+    scanf("%d", &timesCompounded);
+
+    total = principal * pow(1 + rate / timesCompounded, timesCompounded * years);
+
+    printf("After %d year(s), the total will be $%.2lf", years, total);
+
+    return 0;
+}
